08cb10cc971f1c6d7646a962c9bb25fdf9baa42c
NiloyRicky <160626134+NiloyRicky@users.noreply.github.com>
2024-12-04 22:37:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Context from './Context';
+import {Provider} from 'react-redux';
+import store from './store';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <Context>
     <App />
     </Context>
+    </Provider>
     
   </React.StrictMode>
 );
